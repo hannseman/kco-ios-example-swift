ab3712ee7509790c9f25bc27f9fd822c48588a85
Hannes Ljungberg <hannes@5monkeys.se>
2016-09-05 16:50:38 +0200

    Initial Commit

---
+++ kco-mobile-sdk-swift2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kco-mobile-sdk-swift2
+//
+//  Created by Hannes Ljungberg on 05/09/16.
+//  Copyright © 2016 Hannes Ljungberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
